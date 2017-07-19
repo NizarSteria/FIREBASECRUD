8d156ce8f39d9e717e743042a815fa0ec39cc26d
Nizar Aouissaoui <aouissaoui.nizar@gmail.com>
2017-07-19 22:00:14 +0200

    Displaying shopping items Firebase

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AddShoppingPage } from '../add-shopping/add-shopping';
 
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
+import { ShoppingItem } from '../../models/shopping-item/shopping-item-interface';
+
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  shoppingListRef$: FirebaseListObservable<ShoppingItem[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams
+  , private database: AngularFireDatabase) {
+    // Pointing shoppingListRef$ at Firebase -> 'shopping-list' node
+    this.shoppingListRef$ = this.database.list('shopping-list');
+
+    this.shoppingListRef$.subscribe( x => console.log(x));
   }
 
   navigateToAddShoppingPage (){
