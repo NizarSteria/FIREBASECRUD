2f7d74a6dfa40f66848c23fae66ab819debf8299
Nizar Aouissaoui <aouissaoui.nizar@gmail.com>
2017-07-19 21:42:03 +0200

    Adding Shopping to Firebase

--- src/pages/add-shopping/add-shopping.ts
+++ src/pages/add-shopping/add-shopping.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ShoppingItem } from '../../models/shopping-item/shopping-item-interface'
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-add-shopping',
@@ -10,11 +11,36 @@ export class AddShoppingPage {
 
   shoppingItem = {} as ShoppingItem;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  shoppingItemRef$: FirebaseListObservable<ShoppingItem[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private database: AngularFireDatabase
+  ) {
+    this.shoppingItemRef$ = this.database.list('shopping-list');
+    /**
+     * shopping list
+     */
   }
 
-  addShoppingItem(shoppingItem : ShoppingItem){
+  addShoppingItem(shoppingItem: ShoppingItem) {
     console.log(shoppingItem);
+    /**
+     * Create a new anonyms object and convert itemNumber to 
+     * a number.
+     * push this to our Firebase database under the 'shopping-list'
+     * node.
+     */
+    this.shoppingItemRef$.push({
+      itemName: this.shoppingItem.itemName,
+      itemNumber: Number(this.shoppingItem.itemNumber)
+    });
+    /**
+   * Reset our hopping item
+   */
+    this.shoppingItem = {} as ShoppingItem;
+
+    // Navigate the user back to the shopping list page
+    this.navCtrl.pop();
   }
 
 }
